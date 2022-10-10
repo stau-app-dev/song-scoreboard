0a724488ca8d384e8c8b3108004acb87778d6e8e
Martin Sit <46298951+martin226@users.noreply.github.com>
2022-10-10 13:53:10 -0400

    feat: index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,53 @@
 <template>
-  <div></div>
+  <div class="flex min-h-screen flex-col">
+    <Fullscreen v-if="!isFullscreen" />
+    <Header />
+    <div class="flex flex-1 flex-col space-y-4 py-6 px-16">
+      <h3 class="text-center text-4xl font-extralight">
+        Upvote songs on the school app!
+      </h3>
+      <h4 class="text-center text-3xl">
+        The
+        <span class="underline decoration-dotted">top three songs</span> (min.
+        300 votes) will be selected for an upcoming LIVE performance.
+      </h4>
+      <div class="flex-1 py-3 text-center">
+        <Scoreboard />
+        <h5 class="text-2xl font-extralight mt-6 italic">(Live Scoreboard)</h5>
+      </div>
+    </div>
+    <Footer />
+  </div>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  data() {
+    return {
+      windowHeight: window.innerHeight,
+    };
+  },
+  computed: {
+    isFullscreen() {
+      if ((screen.availHeight || screen.height - 50) <= this.windowHeight) {
+        return true;
+      }
+      return false;
+    },
+  },
+  mounted() {
+    this.$nextTick(() => {
+      window.addEventListener('resize', this.onResize);
+    });
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.onResize);
+  },
+  methods: {
+    onResize() {
+      this.windowHeight = window.innerHeight;
+    },
+  },
 };
 </script>
