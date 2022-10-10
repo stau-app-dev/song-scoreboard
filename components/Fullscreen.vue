d3f9576e46234064ad66cf4fe3b041c161a837cc
Martin Sit <46298951+martin226@users.noreply.github.com>
2022-10-10 13:51:04 -0400

    feat: force fullscreen component

---
+++ components/Fullscreen.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    class="fixed bg-primary min-h-full w-full z-50 flex justify-center items-center"
+  >
+    <h1 class="text-6xl text-white font-bold">Click to go to fullscreen.</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FullscreenComponent',
+  mounted() {
+    document.addEventListener('click', () => {
+      const el = document.documentElement;
+      const rfs =
+        el.requestFullScreen ||
+        el.webkitRequestFullScreen ||
+        el.mozRequestFullScreen;
+      rfs.call(el);
+    });
+  },
+};
+</script>
