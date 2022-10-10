739ef2a8a4272eaf2537a86248fc747971932c3f
Martin Sit <46298951+martin226@users.noreply.github.com>
2022-10-10 13:51:45 -0400

    feat: footer component

---
+++ components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    class="flex items-center justify-center bg-gradient-to-r from-red-700 to-primary py-4 px-4"
+  >
+    <h1 class="text-center text-2xl font-bold leading-relaxed text-white">
+      Download the Official School App @
+      <span class="underline">app.staugustinechs.ca</span>
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FooterComponent',
+};
+</script>
