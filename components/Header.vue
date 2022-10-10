4906470256d2a96e70b97615296b935c3ca55e75
Martin Sit <46298951+martin226@users.noreply.github.com>
2022-10-10 13:50:34 -0400

    feat: header component

---
+++ components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="flex items-center justify-center bg-gradient-to-r from-red-700 to-primary py-8 px-4"
+  >
+    <h1 class="text-center text-5xl font-bold leading-relaxed text-white">
+      Do you want to see Mr. Cadawas SING?
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponent',
+};
+</script>
